0fa86c237c5f3997d0389cf67a4def9a566bf578
Katelyn Pace <katelynpace@interapts-macbook-pro-4.local>
2018-09-12 10:07:36 -0500

    Initial Commit

---
+++ Task Manager/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Task Manager
+//
+//  Created by Katelyn Pace on 9/12/18.
+//  Copyright © 2018 Katelyn Pace. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
